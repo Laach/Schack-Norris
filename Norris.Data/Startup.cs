78d44917a7719261272077d53d40dfe4a3a1a5dd
Philip Karlsson <karlsson914@gmail.com>
2019-10-11 12:32:46 +0200

    Added identity to services

--- Norris.Data/Startup.cs
+++ Norris.Data/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Norris.Data.Data;
+using Norris.Data.Data.Entities;
 
 namespace Norris.Data
 {
@@ -24,6 +26,7 @@ namespace Norris.Data
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<NContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<NContext>().AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
