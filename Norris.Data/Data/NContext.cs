c44a0b4a3343fa0f311794b1d61bab9d2b1a2398
Philip Karlsson <karlsson914@gmail.com>
2019-10-11 11:47:33 +0200

    Added the custom context for Norris, NContext.

---
+++ Norris.Data/Data/NContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Norris.Data.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Norris.Data.Data
+{
+    public class NContext : IdentityDbContext<User>
+    {
+        public NContext(DbContextOptions<NContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
