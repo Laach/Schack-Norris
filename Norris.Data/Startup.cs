754735fc52380df53738dad3295820d1d807c3e6
Philip Karlsson <karlsson914@gmail.com>
2019-10-11 12:26:33 +0200

    Added a IConfiguration to Startup.cs that allows loading the connection string from the appsettings.json

--- Norris.Data/Startup.cs
+++ Norris.Data/Startup.cs
@@ -5,12 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Norris.Data
 {
     public class Startup
     {
+        public IConfiguration Configuration;
+        public Startup(IConfiguration conf)
+        {
+            Configuration = conf;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
