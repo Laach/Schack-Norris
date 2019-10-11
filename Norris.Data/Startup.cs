e25a3a79dece7edce896ea1b835fa5267325b812
Philip Karlsson <karlsson914@gmail.com>
2019-10-11 12:29:49 +0200

    Loaded the connection string

--- Norris.Data/Startup.cs
+++ Norris.Data/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Norris.Data.Data;
 
 namespace Norris.Data
 {
@@ -21,6 +23,7 @@ namespace Norris.Data
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
