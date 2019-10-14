31cccbb9cb947fa3d17341c4dab3274463a5367e
Emil <emil.elfstrom99@gmail.com>
2019-10-14 15:49:42 +0200

    Removed links to obsolete site pages
    Links to "About" and "Contact" have been removed.

--- Norris.UI/Controllers/HomeController.cs
+++ Norris.UI/Controllers/HomeController.cs
@@ -26,20 +26,6 @@ namespace Norris.UI.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Lobby()
         {
             ViewData["Message"] = "Lobby page.";
