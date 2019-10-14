6f9b6104a803f26564d7ac895f8867a4fc270c6b
Sorken <spec_89@hotmail.com>
2019-10-14 15:16:15 +0200

    Changed the login and register pages to look and work as specified.

--- Norris.UI/Models/AccountViewModels/RegisterViewModel.cs
+++ Norris.UI/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,11 @@ namespace Norris.UI.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
