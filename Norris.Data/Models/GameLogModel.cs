5df3f20dc7e467efa1c268085b959ce301f2a6e0
Mystige <mystige@hotmail.com>
2019-10-15 07:20:28 +0200

    Added a model representing a Gamelog

--- Norris.Data/Models/GameLogModel.cs
+++ Norris.Data/Models/GameLogModel.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using Norris.Data.Models.Board;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Norris.Data.Models
 {
     public class GameLogModel
     {
+        public List<MoveModel> GameLog { get; set; }
     }
 }
