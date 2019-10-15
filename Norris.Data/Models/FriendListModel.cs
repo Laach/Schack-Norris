886a7bcf9a38f9ae8f34e9f52959e133b3959d02
Mystige <mystige@hotmail.com>
2019-10-15 07:42:52 +0200

    Updated namespace
    Updated Namespace to no longer contain the old "User" folder

--- Norris.Data/Models/FriendListModel.cs
+++ Norris.Data/Models/FriendListModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Norris.Data.Models.Users
+namespace Norris.Data.Models
 {
     public class FriendListModel
     {
