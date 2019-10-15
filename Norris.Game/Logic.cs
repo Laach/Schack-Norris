4ad2d644038ab1f3605478c4bef26404a4f7abc4
Casper Andersson <andersson.casper99@gmail.com>
2019-10-15 12:46:24 +0200

    Added moveset for Pawns and Knights.

--- Norris.Game/Logic.cs
+++ Norris.Game/Logic.cs
@@ -54,7 +54,7 @@ namespace Norris.Game {
       Color player, 
       Point point){
 
-      var arr = new Point[]{
+      var moves = new Point[]{
         new Point(){ Y = point.Y + 1, X = point.X + 1},
         new Point(){ Y = point.Y + 1, X = point.X - 1},
         new Point(){ Y = point.Y - 1, X = point.X + 1},
@@ -65,7 +65,7 @@ namespace Norris.Game {
         new Point(){ Y = point.Y    , X = point.X + 1},
         new Point(){ Y = point.Y    , X = point.X - 1},
       };
-      return arr.Where(xy => Inbounds(xy) && CanGoTo(board, xy, player));
+      return moves.Where(xy => Inbounds(xy) && CanGoTo(board, xy, player));
     }
 
 
@@ -98,6 +98,74 @@ namespace Norris.Game {
       .Concat(BishopMoves(board, player, point));
     }
 
+    public static IEnumerable<Point> KnightMoves(
+      ChessBoard board, 
+      Color player, 
+      Point point){
+
+      var moves = new Point[]{
+        new Point(){ Y = point.Y + 1, X = point.X - 2},
+        new Point(){ Y = point.Y - 1, X = point.X - 2},
+
+        new Point(){ Y = point.Y + 1, X = point.X + 2},
+        new Point(){ Y = point.Y - 1, X = point.X + 2},
+
+        new Point(){ Y = point.Y + 2, X = point.X + 1},
+        new Point(){ Y = point.Y + 2, X = point.X - 1},
+
+        new Point(){ Y = point.Y - 2, X = point.X + 1},
+        new Point(){ Y = point.Y - 2, X = point.X - 1},
+      };
+      return moves.Where(xy => Inbounds(xy) && CanGoTo(board, xy, player));
+    }
+
+    static bool PawnCanAttack(ChessBoard board, Point p, Color c){
+      return board[p] != null && board[p].Piece.Color == c;
+    }
+
+    public static IEnumerable<Point> PawnMoves(
+      ChessBoard board, 
+      Color player, 
+      Point point){
+
+      IEnumerable<Point> list = new List<Point>();
+      if(player == Color.White){
+        Point forward = new Point(){ Y = point.Y - 1, X = point.X};
+        if(board[forward] == null){
+          list = list.Append(forward);
+        }
+
+        var attack1 = new Point(){ Y = point.Y - 1, X = point.X + 1};
+        var attack2 = new Point(){ Y = point.Y - 1, X = point.X - 1};
+
+        if(PawnCanAttack(board, attack1, Color.Black)){
+          list = list.Append(attack1);
+        }
+        if(PawnCanAttack(board, attack2, Color.Black)){
+          list = list.Append(attack2);
+        }
+
+      }
+      else{
+        Point forward = new Point(){ Y = point.Y + 1, X = point.X};
+        if(board[forward] == null){
+          list = list.Append(forward);
+        }
+
+        var attack1 = new Point(){ Y = point.Y + 1, X = point.X + 1};
+        var attack2 = new Point(){ Y = point.Y + 1, X = point.X - 1};
+
+        if(PawnCanAttack(board, attack1, Color.White)){
+          list = list.Append(attack1);
+        }
+        if(PawnCanAttack(board, attack2, Color.White)){
+          list = list.Append(attack2);
+        }
+
+      }
+      return list;
+
+    }
 
     public static IEnumerable<Point> GetMovesFor(
       ChessBoard board, 
@@ -109,6 +177,8 @@ namespace Norris.Game {
         case PieceType.Queen : return QueenMoves (board, player, point);
         case PieceType.Rook  : return RookMoves  (board, player, point);
         case PieceType.Bishop: return BishopMoves(board, player, point);
+        case PieceType.Knight: return KnightMoves(board, player, point);
+        case PieceType.Pawn  : return PawnMoves  (board, player, point);
         default: return new List<Point>();
       }
 
