a82d06543d949b0b61a90f62379d029545262625
Sorken <spec_89@hotmail.com>
2019-10-16 11:24:16 +0200

    Finished working with the lobby. Had to do some changes to the friends logic aswell.

---
+++ Norris.UI/Models/ManageViewModels/LobbyAndFriendsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Norris.Data.Data.Entities;
+
+namespace Norris.UI.Models.ManageViewModels
+{
+    public class LobbyAndFriendsViewModel
+    {
+        public List<User> CurrentLobbyUsers { get; set; }
+        public List<User> Friends { get; set; }
+
+    }
+}
