1a9f2649443e7b1695c7428e3cda23e827319728
Mystige <mystige@hotmail.com>
2019-10-17 19:51:57 +0200

    New migrations for the new database

--- Norris.Data/Migrations/20191017174119_DbSetup.Designer.cs
+++ Norris.Data/Migrations/20191017174119_DbSetup.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Norris.Data.Migrations
 {
     [DbContext(typeof(NContext))]
-    partial class NContextModelSnapshot : ModelSnapshot
+    [Migration("20191017174119_DbSetup")]
+    partial class DbSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
