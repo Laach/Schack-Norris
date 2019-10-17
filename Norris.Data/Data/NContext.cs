cc1ad6f57cfed762933cdd7ca16e4e10c4015fc1
Mystige <mystige@hotmail.com>
2019-10-17 19:51:02 +0200

    Added relations between entities and added a GameSession table

--- Norris.Data/Data/NContext.cs
+++ Norris.Data/Data/NContext.cs
@@ -1,15 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Norris.Data.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Norris.Data.Data
 {
     public class NContext : IdentityDbContext<User>
     {
+        public DbSet<GameSession> GameSessions { get; set; }
         public NContext(DbContextOptions<NContext> options) : base(options)
         {
 
@@ -18,6 +15,28 @@ namespace Norris.Data.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.Entity<Friends>()
+                .HasKey(c => new { c.UserId, c.FriendID });
+            builder.Entity<Friends>()
+                .HasOne(pt => pt.Friend)
+                .WithMany() // <--
+                .HasForeignKey(pt => pt.FriendID)
+                .OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Friends>()
+                .HasOne(pt => pt.User)
+                .WithMany(t => t.Friends)
+                .HasForeignKey(pt => pt.UserId);
+
+            builder.Entity<GameSession>()
+                .HasOne(pt => pt.Player1)
+                .WithMany(pt => pt.WhiteGameSessions)
+                .HasForeignKey(pt => pt.Player1ID);
+            builder.Entity<GameSession>()
+                .HasOne(pt => pt.Player2)
+                .WithMany(pt => pt.BlackGameSessions)
+                .HasForeignKey(pt => pt.Player2ID)
+                .OnDelete(DeleteBehavior.Restrict);
+                
         }
     }
 }
