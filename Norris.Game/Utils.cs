723b8103a11347428ca1c9e96484459a386b0b53
Casper Andersson <andersson.casper99@gmail.com>
2019-10-18 17:43:10 +0200

    Changed public InitBoard to string[ , ]

--- Norris.Game/Utils.cs
+++ Norris.Game/Utils.cs
@@ -180,6 +180,50 @@ namespace Norris.Game {
    
     public static char ColorToChar(Color c) => c == Color.White ? 'W' : 'B';
 
+
+
+
+    
+    public static PieceModel[,] InitBoard(){
+      PieceModel[,] board = new PieceModel[8,8];
+      board[0,0] = Utils.NewPiece(PieceType.Rook, Color.Black);
+      board[0,7] = Utils.NewPiece(PieceType.Rook, Color.Black);
+
+      board[0,1] = Utils.NewPiece(PieceType.Knight, Color.Black);
+      board[0,6] = Utils.NewPiece(PieceType.Knight, Color.Black);
+
+      board[0,2] = Utils.NewPiece(PieceType.Bishop, Color.Black);
+      board[0,5] = Utils.NewPiece(PieceType.Bishop, Color.Black);
+
+      board[0,3] = Utils.NewPiece(PieceType.Queen, Color.Black);
+      board[0,4] = Utils.NewPiece(PieceType.King, Color.Black );
+
+      for(int i = 0; i < 8; i++){
+        board[1,i] = Utils.NewPiece(PieceType.Pawn, Color.Black); 
+      }
+
+
+
+      board[7,0] = Utils.NewPiece(PieceType.Rook, Color.White);
+      board[7,7] = Utils.NewPiece(PieceType.Rook, Color.White);
+
+      board[7,1] = Utils.NewPiece(PieceType.Knight, Color.White);
+      board[7,6] = Utils.NewPiece(PieceType.Knight, Color.White);
+
+      board[7,2] = Utils.NewPiece(PieceType.Bishop, Color.White);
+      board[7,5] = Utils.NewPiece(PieceType.Bishop, Color.White);
+
+      board[7,3] = Utils.NewPiece(PieceType.Queen, Color.White);
+      board[7,4] = Utils.NewPiece(PieceType.King, Color.White );
+
+      for(int i = 0; i < 8; i++){
+        board[6,i] = Utils.NewPiece(PieceType.Pawn, Color.White); 
+      }
+
+      return board;
+
+    }
+
     
   }
 }
