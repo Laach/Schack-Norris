71d2bd8f20af06af4ed0ed266d3a790f479203a7
Casper Andersson <andersson.casper99@gmail.com>
2019-10-21 15:32:57 +0200

    Added RecordGame(..) to save moves, and RunInput() to run from file.

--- Norris.Game/Test.cs
+++ Norris.Game/Test.cs
@@ -27,13 +27,68 @@ namespace Norris.Game{
       Utils.PrettyPrint(board);
     }
 
+    public static ValueTuple<ChessBoard, Color> RunInput(){
+      ChessLogic Chess = new ChessLogic();
+      ChessBoard board = new ChessBoard(Chess.InitBoard());
+      var data = System.IO.File.ReadAllLines("testgame.txt");
+      Color turn = Color.White;
+      foreach(var s in data){
+        if(s == ""){break;}
+        var (from, to) = Utils.MoveToPoints(s);
+        if(!Logic.IsValidMove(board, from, to, turn)){
+          Console.WriteLine("Invalid move: " + s);
+          Utils.PrettyPrint(board);
+          Environment.Exit(1);
+        }
+        board = Logic.DoMove(board, from, to);
+        turn = turn == Color.White ? Color.Black : Color.White;
+      }
+      return (board, turn);
+    }
+
     public static void PlayGame(){
       ChessLogic Chess = new ChessLogic();
       ChessBoard board = new ChessBoard(Chess.InitBoard());
       Color turn = Color.White;
+      // var (board, turn) = RunInput();
       string s = "";
       while(true){
         Utils.PrettyPrint(board);
+        if(Logic.IsCheckMate(board, turn)){
+          System.Console.WriteLine("Winner is " + Utils.GetOppositeColor(turn).ToString());
+        }
+
+        Console.WriteLine($"{turn}, input move: ");
+        s = Console.ReadLine();
+  
+        var (from, to) = Utils.MoveToPoints(s);
+        if(!Logic.IsValidMove(board, from, to, turn )){
+          Console.Clear();
+          Console.WriteLine("Invalid move: " + s);
+          continue;
+        }
+        else{
+          Console.Clear();
+          Console.WriteLine();
+        }
+        board = Logic.DoMove(board, from, to);
+        turn = turn == Color.White ? Color.Black : Color.White;
+      }
+    }
+
+
+    public static void RecordGame(string path){
+      ChessLogic Chess = new ChessLogic();
+      ChessBoard board = new ChessBoard(Chess.InitBoard());
+      Color turn = Color.White;
+      // var (board, turn) = RunInput();
+      string s = "";
+      while(true){
+        Utils.PrettyPrint(board);
+        if(Logic.IsCheckMate(board, turn)){
+          System.Console.WriteLine("Winner is " + Utils.GetOppositeColor(turn).ToString());
+        }
+
         Console.WriteLine($"{turn}, input move: ");
         s = Console.ReadLine();
   
@@ -48,9 +103,13 @@ namespace Norris.Game{
           Console.Clear();
           Console.WriteLine();
         }
+        using (var sw = new StreamWriter(path, true)){
+          sw.WriteLine(s);
+        }
         board = Logic.DoMove(board, from, to);
         turn = turn == Color.White ? Color.Black : Color.White;
       }
+
     }
 
 
