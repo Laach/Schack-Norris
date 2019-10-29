3ac277f8b6ebe14418693be63c0b66c46046b444
Mystige <mystige@hotmail.com>
2019-10-29 11:01:20 +0100

    Added DTO for ArchivedGames

---
+++ Norris.Data/Models/DTO/ArchivedGamesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Norris.Data.Models.DTO
+{
+    public class ArchivedGamesDTO
+    {
+        public string GameID { get; set; }
+        public bool AmIWinner { get; set; }
+        public string OpponentName { get; set; }
+    }
+}
