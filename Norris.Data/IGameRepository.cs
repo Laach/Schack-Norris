de94039dbf0b7169f21048a0bbf9d7e8a01489dc
Mystige <mystige@hotmail.com>
2019-11-01 11:41:30 +0100

    Updated Repository
    Moved in GameRepository och IGameRepository and added and updated DTO

--- Norris.Data/IGameRepository.cs
+++ Norris.Data/IGameRepository.cs
@@ -11,28 +11,28 @@ namespace Norris.Data
     {
         //Expects a gameid and returns the boardstate,a log and the color the of the active player
         GameStateDTO GetGamestate(string id);
-        UserFriendsDTO GetFriendList(string userID);      
+        UserFriendsDTO GetFriendList(string userID);
         UserListDTO GetUserSearchResult(string userID, string searchterm);
         ViewUserModel GetUserData(string userID);
         GameStateDTO AddNewMove(NewMoveDTO newMove);
         string AddNewGame(string player1ID, string player2ID);
         bool AddFriend(string currentUserID, string friendUserID);
         UserListDTO GetPlayerLobby();
-        void EnterLobby(string userID);
-        void LeaveLobby(string userID);
-        bool IsInLobby(string userID);
         IEnumerable<UserActiveGamesDTO> GetUserGameList(string userID);
         bool IsActivePlayer(string gameID, string userID);
         char GetPlayerColor(string gameID, string userID);
+        void EnterLobby(string userID);
+        void LeaveLobby(string userID);
+        bool IsInLobby(string userID);
+        bool AddChatMessage(ChatMessageDTO chatMessage, string GameID);
+        IEnumerable<ChatMessageDTO> GetMessageLog(string GameID);
+        int GetMessageLogLenght(string GameID);
+        string GetUserNameFromId(string UserID);
+        IEnumerable<ArchivedGamesDTO> GetArchivedGameList(string userID);
+        IEnumerable<UserActiveGamesDTO> GetAllGames(string userID);
+        void SetGameToFinished(string GameID);
         IEnumerable<string> GetChangedTiles(string gameID);
         void SetChangedTiles(string gameID, IEnumerable<string> changedtiles);
-
-        bool AddChatMessage(ChatMessageDTO chatMessage, string GameID);
-
-        IEnumerable<ChatMessageDTO> GetMessageLog(string GameID);
-
-        int GetMessageLogLenght(string GameID);
-
-        string GetUserNameFromId(string UserID);
     }
+
 }
